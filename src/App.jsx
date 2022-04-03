f6d71bea231e855cb57de32604b9b4823c79693e
aniOfri <46932810+aniOfri@users.noreply.github.com>
2022-04-03 06:28:09 +0200

    finished basic code -- time for some css

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,49 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import SettlementsList from './data/settlements.json';
 
-function randInt(max) {
+function RandInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-function getSettlement(){
+function GetSettlement(settle){
   let newSettlement;
-  newSettlement = SettlementsList[randInt(SettlementsList.length)];
-
-  return newSettlement;
+  newSettlement = SettlementsList[RandInt(SettlementsList.length)];
+  return [settle, newSettlement];
 }
 
 function App() {
-  const [firstSettlement, setSettlement1] = useState(SettlementsList[0]);
-  const [secondSettlement, setSettlement2] = useState(SettlementsList[0]);
+  const [settlements, setSettlements] = useState(GetSettlement(SettlementsList[RandInt(SettlementsList.length)]));
+  const [streak, setStreak] = useState(0);
+
+  function Choice(north){
+    let top = settlements[0].gps.split(" ")[1].replace(")", "");
+    let bottom = settlements[1].gps.split(" ")[1].replace(")", "");
+
+    if (north && top < bottom)
+      setStreak(streak + 1);
+    else if (!north && top > bottom)
+      setStreak(streak + 1);
+    else
+      setStreak(0);
+
+    setSettlements(GetSettlement(settlements[1]))
+  }
   
   return (
     <div className="App">
       <header className="App-header">
         <p>The Souther Norther Game</p>
+        <p>Streak: {streak}</p>
+        <h1>{settlements[0].cityLabel}</h1>
         <p>
-          <button type="button" onClick={() => {setSettlement1(getSettlement())}}>
-            Settlement
+        <button type="button" onClick={() => {Choice(true)}}>
+            Norther
           </button>
-        </p>
-        <h1>{firstSettlement.cityLabel}</h1>
-        <h1>{firstSettlement.gps.split(" ")[1].replace(")", "")}</h1>
+          <button type="button" onClick={() => {Choice(false)}}>
+            Souther
+          </button>
+          </p>
+        <h1>{settlements[1].cityLabel}</h1>
       </header>
     </div>
   )
