5a027e984c8e0663268f75d643e2e5e01c59c206
aniOfri <46932810+aniOfri@users.noreply.github.com>
2022-04-25 07:27:40 +0200

    change of vite config (not working)

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		host: "0.0.0.0",
-		port: 3000,
-		strictPort: true,
-		hmr: {
-			port: 443,
-		},
-	}
-  
+		  https: true,
+		  hmr: {
+			host: '0.0.0.0',
+			port: 3000,
+			protocol: 'wss'
+		  }
+		}
 })
